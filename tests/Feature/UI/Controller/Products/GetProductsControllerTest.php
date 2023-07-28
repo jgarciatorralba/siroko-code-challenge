d653fd59e8858a248ec626ba5921ac0cbb68e008
Jorge García <jgarciatorralba@gmail.com>
2023-07-28 19:25:38 +0200

    create first feature test

---
+++ tests/Feature/UI/Controller/Products/GetProductsControllerTest.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+use Symfony\Component\HttpFoundation\Response;
+
+it('should return a list of products', function () {
+    $client = $this->getApiClient();
+    $response = $client->request('GET', '/api/products');
+    $content = $response->toArray();
+
+    expect($response->getStatusCode())->toEqual(Response::HTTP_OK)
+        ->and($content)->toBeArray()
+        ->and($content)->toHaveCount(10)
+        ->and($content[0])->toHaveKeys(['id', 'name', 'price', 'created_at', 'updated_at'])
+        ->and($content[0]['id'])->toBeString()
+        ->and($content[0]['name'])->toBeString()
+        ->and($content[0]['price'])->toBeFloat()
+        ->and(new DateTimeImmutable($content[0]['created_at']))->toBeInstanceOf(DateTimeImmutable::class)
+        ->and(new DateTimeImmutable($content[0]['updated_at']))->toBeInstanceOf(DateTimeImmutable::class);
+
+});
