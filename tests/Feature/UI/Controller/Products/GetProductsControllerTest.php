4198ab246f39f9639db46bf18f1ff086e18bd323
Jorge García <jgarciatorralba@gmail.com>
2023-07-29 16:54:08 +0200

    update feature test

--- tests/Feature/UI/Controller/Products/GetProductsControllerTest.php
+++ tests/Feature/UI/Controller/Products/GetProductsControllerTest.php
@@ -7,15 +7,18 @@ use Symfony\Component\HttpFoundation\Response;
 it('should return a list of serialized products', function () {
     $client = $this->getApiClient();
     $response = $client->request('GET', '/api/products');
-    $content = $response->toArray();
+    $decodedResponse = $response->toArray();
 
     expect($response->getStatusCode())->toEqual(Response::HTTP_OK)
-        ->and($content)->toBeArray()
-        ->and($content)->toHaveCount(10)
-        ->and($content[0])->toHaveKeys(['id', 'name', 'price', 'created_at', 'updated_at'])
-        ->and($content[0]['id'])->toBeString()
-        ->and($content[0]['name'])->toBeString()
-        ->and($content[0]['price'])->toBeFloat()
-        ->and(new DateTimeImmutable($content[0]['created_at']))->toBeInstanceOf(DateTimeImmutable::class)
-        ->and(new DateTimeImmutable($content[0]['updated_at']))->toBeInstanceOf(DateTimeImmutable::class);
+        ->and($response->getContent())->toBeJson()
+        ->and($decodedResponse)->toBeArray()
+        ->and($decodedResponse)->toHaveCount(10)
+        ->and($decodedResponse[rand(0, 9)])->toHaveKeys(['id', 'name', 'price', 'created_at', 'updated_at'])
+        ->and($decodedResponse[rand(0, 9)]['id'])->toBeString()
+        ->and($decodedResponse[rand(0, 9)]['name'])->toBeString()
+        ->and(floatval($decodedResponse[rand(0, 9)]['price']))->toBeFloat()
+        ->and(new DateTimeImmutable($decodedResponse[0]['created_at']))
+            ->toBeInstanceOf(DateTimeImmutable::class)
+        ->and(new DateTimeImmutable($decodedResponse[0]['updated_at']))
+            ->toBeInstanceOf(DateTimeImmutable::class);
 });
