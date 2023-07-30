c29afe59fa9ee1b3020299338e86e559eebd92aa
Jorge García <jgarciatorralba@gmail.com>
2023-07-30 11:17:35 +0200

    add test for service GetProductById

---
+++ tests/Unit/Products/Domain/Service/GetProductByIdTest.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+use App\Products\Domain\Exception\ProductNotFoundException;
+use App\Products\Domain\Product;
+use App\Products\Domain\Service\GetProductById;
+use App\Shared\Domain\ValueObject\Uuid;
+use App\Tests\Unit\Products\TestCase\ProductRepositoryMock;
+
+beforeEach(function () {
+    $this->productRepositoryMock = new ProductRepositoryMock($this);
+});
+
+it('should return a product', function () {
+    $id = Uuid::random();
+    $product = Product::create(
+        id: $id,
+        name: 'test-product',
+        price: 1.23
+    );
+
+    $this->productRepositoryMock->shouldFindProductById($id, $product);
+
+    $service = new GetProductById(
+        productRepository: $this->productRepositoryMock->getMock()
+    );
+    $result = $service->__invoke($id);
+
+    expect($result)->toEqual($product);
+});
+
+it('should throw an exception if the product is not found', function () {
+    $id = Uuid::random();
+
+    $this->productRepositoryMock->shouldNotFindProductById($id);
+
+    $service = new GetProductById(
+        productRepository: $this->productRepositoryMock->getMock()
+    );
+    $service->__invoke($id);
+})->throws(ProductNotFoundException::class);
