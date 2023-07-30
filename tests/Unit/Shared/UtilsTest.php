ae8560be1698ee1410d661688e68d68f749e7748
Jorge García <jgarciatorralba@gmail.com>
2023-07-30 10:53:47 +0200

    add unit test for method "stringToDate"

--- tests/Unit/Shared/UtilsTest.php
+++ tests/Unit/Shared/UtilsTest.php
@@ -6,6 +6,8 @@ namespace App\Tests\Unit\Shared;
 
 use App\Shared\Utils;
 use DateTime;
+use DateTimeInterface;
+use Exception;
 
 describe('dateToString', function () {
     it('should convert a DateTime object to string', function () {
@@ -16,4 +18,20 @@ describe('dateToString', function () {
             ->toBeString()
             ->toMatch('/\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2]\d|3[0-1])T[0-2]\d:[0-5]\d:[0-5]\d[+-][0-2]\d:[0-5]\d/');
     });
+});
+
+describe('stringToDate', function () {
+    it('should convert a valid string to a DateTime object', function () {
+        $string = '2023-08-01';
+        $date = new DateTime($string);
+
+        expect($date)
+            ->toBeInstanceOf(DateTimeInterface::class);
+    });
+
+    it('should throw an exception with an invalid string', function () {
+        $string = 'invalid-string';
+
+        Utils::stringToDate($string);
+    })->throws(Exception::class);
 });
