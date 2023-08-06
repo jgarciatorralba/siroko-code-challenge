ab542c634f3bee174f916667c3fe4f1f56713579
Jorge García <jgarciatorralba@gmail.com>
2023-08-06 21:28:42 +0200

    update unit test for GetProductByIdQueryHandler

--- tests/Unit/Products/Application/Query/GetProductById/GetProductByIdQueryHandlerTest.php
+++ tests/Unit/Products/Application/Query/GetProductById/GetProductByIdQueryHandlerTest.php
@@ -3,6 +3,7 @@
 declare(strict_types=1);
 
 use App\Products\Application\Query\GetProductById\GetProductByIdQueryHandler;
+use App\Products\Domain\Exception\ProductNotFoundException;
 use App\Products\Domain\Product;
 use App\Shared\Domain\ValueObject\Uuid;
 use App\Tests\Unit\Products\Application\Query\GetProductById\GetProductByIdQueryMother;
@@ -14,14 +15,12 @@ beforeEach(function () {
 });
 
 it('should return a normalized product given its id', function () {
-    $now = new DateTimeImmutable();
-
     $product = Product::create(
         id: Uuid::random(),
         name: 'get-product-unit-test',
         price: 1.23,
-        createdAt: $now,
-        updatedAt: $now
+        createdAt: new DateTimeImmutable(),
+        updatedAt: new DateTimeImmutable()
     );
 
     $query = GetProductByIdQueryMother::create(
@@ -38,3 +37,27 @@ it('should return a normalized product given its id', function () {
 
     expect($result)->toBeQueryResponse($response->data());
 });
+
+it('should throw an exception if a product is not found', function () {
+    $product = Product::create(
+        id: Uuid::random(),
+        name: 'get-product-unit-test',
+        price: 1.23,
+        createdAt: new DateTimeImmutable(),
+        updatedAt: new DateTimeImmutable()
+    );
+
+    $query = GetProductByIdQueryMother::create(
+        id: $product->id()->value()
+    );
+    $response = GetProductByIdResponseMother::create($product);
+
+    $this->getProductById->shouldThrowException($product->id());
+
+    $handler = new GetProductByIdQueryHandler(
+        getProductById: $this->getProductById->getMock()
+    );
+    $result = $handler->__invoke($query);
+
+    expect($result)->toBeQueryResponse($response->data());
+})->throws(ProductNotFoundException::class);
