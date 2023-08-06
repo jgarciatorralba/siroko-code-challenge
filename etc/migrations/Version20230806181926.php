3ca08b78e56e2c934361228fb7c9b1d88cd9c93e
Jorge García <jgarciatorralba@gmail.com>
2023-08-06 20:22:24 +0200

    update migration

--- etc/migrations/Version20230806181926.php
+++ etc/migrations/Version20230806181926.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20230806122528 extends AbstractMigration
+final class Version20230806181926 extends AbstractMigration
 {
     public function getDescription(): string
     {
