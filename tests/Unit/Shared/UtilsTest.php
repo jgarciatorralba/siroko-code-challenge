22b3b00a69763fa367bc369859fa606c2cfee3f8
Jorge García <jgarciatorralba@gmail.com>
2023-08-08 23:02:42 +0200

    complete unit test for Utils class

--- tests/Unit/Shared/UtilsTest.php
+++ tests/Unit/Shared/UtilsTest.php
@@ -35,3 +35,22 @@ describe('stringToDate', function () {
         Utils::stringToDate($string);
     })->throws(Exception::class);
 });
+
+describe('extractClassName', function () {
+    it('should extract the class name from a class', function () {
+        $className = Utils::extractClassName(Utils::class);
+        expect($className)->toBe('Utils');
+    });
+});
+
+describe('toSnakeCase', function () {
+    it('converts camelCase to snake_case', function () {
+        $snakeCase = Utils::toSnakeCase('camelCaseString');
+        expect($snakeCase)->toBe('camel_case_string');
+    });
+
+    it('converts PascalCase to snake_case', function () {
+        $snakeCase = Utils::toSnakeCase('PascalCaseString');
+        expect($snakeCase)->toBe('pascal_case_string');
+    });
+});
