5d92d56a97d6ecd3044d9872f4b3261f75042028
Jorge García <jgarciatorralba@gmail.com>
2023-08-11 16:48:42 +0200

    create service to retrieve products in one query

--- src/Carts/Application/Command/CreateCart/CreateCartCommandHandler.php
+++ src/Carts/Application/Command/CreateCart/CreateCartCommandHandler.php
@@ -7,37 +7,46 @@ namespace App\Carts\Application\Command\CreateCart;
 use App\Carts\Domain\Cart;
 use App\Carts\Domain\CartItem;
 use App\Carts\Domain\Service\CreateCart;
-use App\Products\Domain\Service\GetProductById;
+use App\Products\Domain\Product;
+use App\Products\Domain\Service\GetProductsById;
 use App\Shared\Domain\Bus\Command\CommandHandler;
 use App\Shared\Domain\ValueObject\Uuid;
 
 final class CreateCartCommandHandler implements CommandHandler
 {
     public function __construct(
-        private readonly GetProductById $getProductById,
+        private readonly GetProductsById $getProductsById,
         private readonly CreateCart $createCart
     ) {
     }
 
     public function __invoke(CreateCartCommand $command): void
     {
+        $productIds = [];
+        /** @var array<string, string|int> $commandCartItem */
+        foreach ($command->items() as $commandCartItem) {
+            $productIds[] = Uuid::fromString($commandCartItem['productId']);
+        }
+
+        $products = $this->getProductsById->__invoke($productIds);
+
+        $mappedProducts = [];
+        foreach ($products as $product) {
+            $mappedProducts[$product->id()->value()] = $product;
+        }
+
         $cart = Cart::create(
             id: Uuid::fromString($command->id()),
             createdAt: $command->createdAt(),
             updatedAt: $command->updatedAt()
         );
 
-        //TODO: get all products in one query
-
         /** @var array<string, string|int> $commandCartItem */
         foreach ($command->items() as $commandCartItem) {
-            $productId = Uuid::fromString($commandCartItem['productId']);
-            $product = $this->getProductById->__invoke($productId);
-
             $cartItem = CartItem::create(
                 id: Uuid::fromString($commandCartItem['id']),
                 cart: $cart,
-                product: $product,
+                product: $mappedProducts[$commandCartItem['productId']],
                 quantity: $commandCartItem['quantity'],
                 createdAt: $command->createdAt(),
                 updatedAt: $command->updatedAt()
---
+++ src/Products/Domain/Service/GetProductsById.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Products\Domain\Service;
+
+use App\Shared\Domain\ValueObject\Uuid;
+use App\Products\Domain\Contract\ProductRepository;
+use App\Products\Domain\Exception\ProductNotFoundException;
+use App\Products\Domain\Product;
+
+final class GetProductsById
+{
+    public function __construct(
+        private readonly ProductRepository $productRepository
+    ) {
+    }
+
+    /**
+     * @param Uuid[] $ids
+     * @return Product[]
+     */
+    public function __invoke(array $ids): array
+    {
+        $products = $this->productRepository->findByCriteria(['id' => $ids]);
+        $missingProducts = array_diff(
+            array_map(fn (Uuid $id) => $id->value(), $ids),
+            array_map(fn (Product $product) => $product->id()->value(), $products)
+        );
+
+        if (!empty($missingProducts)) {
+            throw new ProductNotFoundException(
+                Uuid::fromString(reset($missingProducts))
+            );
+        }
+
+        return $products;
+    }
+}
