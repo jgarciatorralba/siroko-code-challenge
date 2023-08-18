97435d454cd5d8b9869045f8b2333360b938e72f
Jorge García <jgarciatorralba@gmail.com>
2023-08-18 18:38:20 +0200

    unify exceptions format

--- src/Shared/Domain/Assert.php
+++ src/Shared/Domain/Assert.php
@@ -22,7 +22,7 @@ final class Assert
     {
         if (!$item instanceof $class) {
             throw new InvalidArgumentException(sprintf(
-                'The object <%s> is not an instance of <%s>.',
+                "The object '%s' is not an instance of '%s'.",
                 $class,
                 $item::class
             ));
